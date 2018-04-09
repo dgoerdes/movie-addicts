c21a957bb4d22958515ac7a6ee3f5eadf82f2a0a
Daniel Goerdes <d.goerdes@emakina.at>
2018-04-09 12:00:07 +0200

    do some simple header

--- react/src/components/Header/Header.js
+++ react/src/components/Header/Header.js
@@ -1,15 +1,54 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-import styles from './Header.module.scss';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import { Activity } from 'react-feather';
+import s from './Header.module.scss';
 
-const Header = () => {
+const links = [
+    {
+        url: '/movies',
+        componentName: 'Movies',
+        label: 'Movies',
+    },
+    {
+        url: '/tv-shows',
+        componentName: 'TvShows',
+        label: 'TV Shows',
+    }
+];
+
+const Header = ({ className, activePage }) => {
     return (
-        <div className={styles.wrapper}>
-            ...
-        </div>
+        <header className={classNames(className, s.wrapper)}>
+            <nav className={s.nav}>
+                <a href="/" className={s.brand}>
+                    <Activity className={s.brand__logo} />
+                    <span className={s.brand__name}>
+                        Movie Addicts
+                    </span>
+                </a>
+
+                <ul className={s.linkList}>
+                    {links.map((link) => (
+                        <li
+                            key={link.url}
+                            className={classNames(
+                                s.linkList__item,
+                                { [s['-active']]: activePage === link.componentName },
+                            )}
+                        >
+                            <a href={link.url}>{link.label}</a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
     );
 };
 
-Header.propTypes = {};
+Header.propTypes = {
+    activePage: PropTypes.string,
+    className: PropTypes.string,
+};
 
 export default Header;
