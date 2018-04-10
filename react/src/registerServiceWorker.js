b495c00af805deecffb255d1a0b0d1cd77f6d67c
Daniel Goerdes <d.goerdes@emakina.at>
2018-04-10 16:30:06 +0200

    add eslint comments

--- react/src/registerServiceWorker.js
+++ react/src/registerServiceWorker.js
@@ -5,8 +5,10 @@ const registerServiceWorker = () => {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/service-worker.js')
             .then(registration => {
+                // eslint-disable-next-line no-console
                 console.log('SW registered: ', registration);
             }).catch(registrationError => {
+                // eslint-disable-next-line no-console
                 console.log('SW registration failed: ', registrationError);
             });
     });
